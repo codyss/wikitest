d84da48f65c55dbda62501d50a16c56d6abdf188
Chris de Villiers <cpdevill@gmail.com>
2016-01-26 20:50:45 -0500

    Added test framework/skeleton

--- test/itsalive.js
+++ test/itsalive.js
@@ -45,6 +45,39 @@ describe('for each calls callback each time', function () {
     });
 })
 
+describe('Page model', function() {
+
+    describe('Validations', function() {
+        xit('errors without title', function() {});
+        xit('errors without content', function() {});
+    });
+
+    describe('Statics', function() {
+        describe('findByTag', function() {
+            xit('gets pages with the search tag', function() {});
+            xit('does not get pages without the search tag', function() {});
+        });
+    });
+
+    describe('Methods', function() {
+        describe('findSimilar', function() {
+            xit('never gets itself', function() {});
+            xit('gets other pages with any common tags', function() {});
+            xit('does not get other pages without any common tags', function() {});
+        });
+    });
+
+    describe('Virtuals', function() {
+        describe('route', function() {
+            xit('returns the url_name prepended by "/wiki/"', function() {});
+        });
+    });
+
+    describe('Hooks', function() {
+        xit('it sets urlTitle based on title before validating', function() {});
+    });
+
+});
 
 
 
